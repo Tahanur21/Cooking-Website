cfd244522eef417b4cbcc24e8468db3d7c479258
Tahanur Islam <tahanur373@gmail.com>
2025-09-09 07:14:20 +0600

    creating project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    {/* Mother components */}
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import './index.css'
+import App from './App.jsx'
+import { StrictMode } from 'react';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
